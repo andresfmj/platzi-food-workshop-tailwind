d19fe1a9c2c6354b2cf2bbfadbaa2f2b8221fe9b
Andres Fernando <andres.fernando08@gmail.com>
2020-12-25 12:08:45 -0500

    setup tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      display: ['Gilroy', 'sans-serif'],
+      body: ['Graphik', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        cyan: '#9cdbff'
+      },
+      margin: {
+        '96': '24rem',
+        '128': '32rem'
+      }
+    },
+  },
+  variants: {
+    extend: {
+      opacity: ['responsive', 'hover']
+    },
+  },
+  plugins: [],
+}
